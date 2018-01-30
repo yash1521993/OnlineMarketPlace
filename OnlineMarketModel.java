2a3f15aff291f4d3b0a76ce0d3bd0fc6a814aa2a
Kuruganti, Yashwanth Reddy <yashkuru@iupui.edu>
2018-01-29 19:16:27 -0500

    changes to model and interface

--- OnlineMarketModel.java
+++ OnlineMarketModel.java
@@ -16,18 +16,20 @@ import java.rmi.server.UnicastRemoteObject;
  */
 public class OnlineMarketModel {
 	
-
 	public String registerCustomer() throws RemoteException{
 		//yet to implement
-		return "Registration page";
+		System.out.println("Registration page. Register here");
+		return "Registered";
 	}
 
+	
+	
 	public static void main(String args[]) {
 		// Set the RMI Security Manager...
 		System.setSecurityManager(new SecurityManager());
 		
 		try {
-			System.out.println("Creating a Bank Server!");
+			System.out.println("You are now entering Online Market Place");
 			
 			// Location of BankServer
 			String name = "//tesla.cs.iupui.edu:5555/home/yashkuru/OOAD/Assignment1/onlineMarketServer";
@@ -35,17 +37,16 @@ public class OnlineMarketModel {
 			// Create a new instance of a BankServer.
 			OnlineMarketController bank = new OnlineMarketController(name);
 			
-			System.out.println("BankServer: binding it to name: " + name);
+			System.out.println("Reaching server:" + name);
 			
 			// Binds the BankServer to the RMI Service.
 			Naming.rebind(name, bank);
 			
-			System.out.println("Bank Server Ready!");
-		} catch (Exception e){
+			System.out.println("Interface is Ready!You can register, login and shop");
+		} 
+		catch (Exception e){
 			System.out.println("Exception: " + e.getMessage());
 			e.printStackTrace();
 		}
 	}
 }
-
-
