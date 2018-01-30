3680c1e966544a44215f19e217ec65fc4d03443e
Kuruganti, Yashwanth Reddy <yashkuru@iupui.edu>
2018-01-29 19:42:26 -0500

    added comments

--- OnlineMarketView.java
+++ OnlineMarketView.java
@@ -7,30 +7,30 @@
 
 import java.rmi.Naming;
 
-/**
- * BankClient - Value in the 'name' variable should be the location
- * of the BankServer.
- *
+/*
+ *	OnlineMarketView - This acts as a client for rmi communication and 
+ *	view to present the model's data
  */
 public class OnlineMarketView {
 	public static void main(String args[]){
-		// RMI Security Manager
+		// creates a security manager for RMI
 		System.setSecurityManager(new SecurityManager( ));
-		
-		int id;
+		//variable declaration
+		int regId;
 		String register;
+		//try and catch block for exception handling
 		try{
+			// Locate Online Market Server
 			String name = "//tesla.cs.iupui.edu:5555/home/yashkuru/OOAD/Assignment1/onlineMarketServer";
-			// Attempt to locate the BankServer...
-			OnlineMarket myBank = (OnlineMarket) Naming.lookup(name);
-			id = myBank.get_id();
-			
-			register=myBank.registerCustomer();
-			
-			System.out.println("Transaction ID: " + id+register);	
+			OnlineMarket marketApp = (OnlineMarket) Naming.lookup(name);
+			//calling interface implemented methods
+			regId = marketApp.regId();
+			register=marketApp.registerCustomer();
+			System.out.println("Registration ID: " + regId);	
+			System.out.println("Registration Status: "+register);
 		} 
 		catch(Exception e){
-			System.out.println("BankClient Exception: " +e.getMessage());
+			System.out.println("Online Market App Exception: " +e.getMessage());
 			e.printStackTrace();
 		}
 		
