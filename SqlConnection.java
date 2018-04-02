b71f7cb4e704dd01b1cdf70148fe1df0c1959b37
yash1521993 <yash1521993@gmail.com>
2018-04-02 19:07:52 -0400

    setting properties

--- SqlConnection.java
+++ SqlConnection.java
@@ -11,9 +11,24 @@ import java.util.Properties;
 import java.sql.Connection;
 
 public class SqlConnection {
-	
-	private static final String Dbdriver = "com.mysql.jdbc.Driver";
-    private static final String ConnString = "jdbc:mysql://localhost:3306/yashkuru_db";
-    private static final String ConnId = "";
-	private static final String ConnPwd = "";
+
+	private Properties property;
+	private Connection conn;
+
+	private static final String dbDriver = "com.mysql.jdbc.Driver";
+    private static final String connString = "jdbc:mysql://localhost:3306/yashkuru_db";
+    private static final String connId = "";
+    private static final String connPwd = "";
+
+    private Properties setProperties() {
+        if (property == null) {
+            property = new Properties();
+            property.setProperty("yashkuru", connId);
+            property.setProperty("yashkuru", connPwd);
+        }
+        return property;
+    }
+
+    
+
 }
