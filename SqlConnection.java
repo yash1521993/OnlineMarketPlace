dce2d17be5241c5a859573c25e17377eec766d74
yash1521993 <yash1521993@gmail.com>
2018-04-02 22:15:36 -0400

    added comments

--- SqlConnection.java
+++ SqlConnection.java
@@ -5,27 +5,34 @@
 //
 // yashkuru
 
+//importing required packages to perform database operations
 import java.sql.SQLException;
 import java.sql.DriverManager;
 import java.util.Properties;
 import java.sql.Connection;
 
+/*
+	SqlConnection class contains code realted to mysql connection details
+*/
 public class SqlConnection {
-
-	private Properties property;
+	//variable for Connection
 	private Connection conn;
-
+	//static variables initialized to respective url and connection details
 	private static final String dbDriver = "com.mysql.jdbc.Driver";
     private static final String connString = "jdbc:mysql://localhost:3306/yashkuru_db";
     private static final String connId = "yashkuru";
     private static final String connPwd = "yashkuru";
 
-    //
+    //connectMySql() returns a connection instance
     public Connection connectMySql() {
+    	//create a new connection only if there isn't any existing one
         if (conn == null) {
+        	//exception handling block
             try {
+            	//lookup for sql driver
                 Class.forName(dbDriver);
                 conn = DriverManager.getConnection(connString, connId, connPwd);
+                System.out.println("Connecting to Market App Database........");
             } 
             catch (ClassNotFoundException | SQLException e) {
                 e.printStackTrace();
@@ -34,10 +41,10 @@ public class SqlConnection {
         return conn;
     }
 
-    //
+    //closeConnection() method closes the current sql connection
     public void closeConnection() {
+    	//if there is an existing connection, close this
         if (conn != null) {
-
             try {
                 conn.close();
                 conn = null;
