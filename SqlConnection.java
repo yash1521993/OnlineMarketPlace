9752eae44521fee76dd481a4b0ac7497a29d53df
yash1521993 <yash1521993@gmail.com>
2018-04-02 22:06:55 -0400

    setting userid and password in DriverManager

--- SqlConnection.java
+++ SqlConnection.java
@@ -17,24 +17,15 @@ public class SqlConnection {
 
 	private static final String dbDriver = "com.mysql.jdbc.Driver";
     private static final String connString = "jdbc:mysql://localhost:3306/yashkuru_db";
-    private static final String connId = "";
-    private static final String connPwd = "";
-
-    private Properties setProperties() {
-        if (property == null) {
-            property = new Properties();
-            property.setProperty("yashkuru", connId);
-            property.setProperty("yashkuru", connPwd);
-        }
-        return property;
-    }
+    private static final String connId = "yashkuru";
+    private static final String connPwd = "yashkuru";
 
     //
     public Connection connectMySql() {
         if (conn == null) {
             try {
                 Class.forName(dbDriver);
-                conn = DriverManager.getConnection(connString, setProperties());
+                conn = DriverManager.getConnection(connString, connId, connPwd);
             } 
             catch (ClassNotFoundException | SQLException e) {
                 e.printStackTrace();
@@ -46,7 +37,7 @@ public class SqlConnection {
     //
     public void closeConnection() {
         if (conn != null) {
-        	
+
             try {
                 conn.close();
                 conn = null;
