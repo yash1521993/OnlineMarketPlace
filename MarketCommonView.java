245e0d95c9046189f46db636d8a7f86c404bf4c1
yash1521993 <yash1521993@gmail.com>
2018-04-16 01:26:14 -0400

    added generic registration

--- MarketCommonView.java
+++ MarketCommonView.java
@@ -6,16 +6,45 @@
 // yashkuru
 
 import java.util.Scanner;
-
+import java.util.ArrayList;
 /*
 	MarketCommonView - this provides a common user interface for
 	both admin and login. These details are captured .
 */
 public class MarketCommonView{
+
+	//captures user's registration details
+	public ArrayList<String> getRegDetails(){
+		ArrayList<String> userDetails=new ArrayList<String>();
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("~~~~~~~~~~~~~Registration~~~~~~~~~~~~~~~");
+		System.out.println("Enter 'Admin' without quotes for Admin registration");
+		System.out.println("Enter 'Customer' without quotes for Customer registration");
+		System.out.println("--------------Enter one from above------------------");
+		String regType = scanner.nextLine();
+		System.out.println("Enter First Name:");
+		String firstName = scanner.nextLine();
+		System.out.println("Enter Last Name:");
+		String lastName = scanner.nextLine();
+		System.out.println("Enter Login ID:");
+		String inputLoginId = scanner.nextLine();
+		System.out.println("Enter Password:");
+		String inputLoginPwd = scanner.nextLine();
+
+		userDetails.add(regType);
+		userDetails.add(firstName);
+		userDetails.add(lastName);
+		userDetails.add(inputLoginId);
+		userDetails.add(inputLoginPwd);
+		return(userDetails);
+		//return loginType;
+	}
+
 	//captures user type who's trying to access market app
 	public String getLoginType(){
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~Login~~~~~~~~~~~~~~~~~~~~~~~");
 		System.out.println("Enter 'Admin' for Administrator login without quotes");
 		System.out.println("Enter 'Customer' for Customer login without quotes");
 		System.out.println("------------Enter one from above------------------");
