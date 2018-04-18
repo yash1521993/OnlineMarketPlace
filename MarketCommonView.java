1bbb99dffb66dc84b6bf451bed22aec2ffffffa7
yash1521993 <yash1521993@gmail.com>
2018-04-18 00:23:48 -0400

    added user info capturing methods

--- MarketCommonView.java
+++ MarketCommonView.java
@@ -13,32 +13,42 @@ import java.util.ArrayList;
 */
 public class MarketCommonView{
 
-	//captures user's registration details
-	public ArrayList<String> getRegDetails(){
-		ArrayList<String> userDetails=new ArrayList<String>();
+	//allows login or registration
+	public String userAction(){
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("~~~~~~~~~~~~~Login/Registration~~~~~~~~~~~~~~~");
+		System.out.println("Enter 'Login' without quotes for Login");
+		System.out.println("Enter 'Register' without quotes for registration a new account");
+		System.out.println("--------------Enter one from above------------------");
+		String userAction = scanner.nextLine();
+		return userAction;
+	}
 
+	//captures user type who's trying to register with market app
+	public String getRegType(){
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("~~~~~~~~~~~~~Registration~~~~~~~~~~~~~~~");
 		System.out.println("Enter 'Admin' without quotes for Admin registration");
 		System.out.println("Enter 'Customer' without quotes for Customer registration");
 		System.out.println("--------------Enter one from above------------------");
 		String regType = scanner.nextLine();
+		return regType;
+	}
+
+	//captures user's first name details
+	public String getFirstName(){
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter First Name:");
 		String firstName = scanner.nextLine();
+
+	}
+
+	//captures user's last name details
+	public String getLastName(){
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter Last Name:");
 		String lastName = scanner.nextLine();
-		System.out.println("Enter Login ID:");
-		String inputLoginId = scanner.nextLine();
-		System.out.println("Enter Password:");
-		String inputLoginPwd = scanner.nextLine();
 
-		userDetails.add(regType);
-		userDetails.add(firstName);
-		userDetails.add(lastName);
-		userDetails.add(inputLoginId);
-		userDetails.add(inputLoginPwd);
-		return(userDetails);
-		//return loginType;
 	}
 
 	//captures user type who's trying to access market app
@@ -59,6 +69,7 @@ public class MarketCommonView{
 		String inputLoginId = scanner.nextLine();
 		return inputLoginId;
 	}
+
 	//capturing password
 	public String getInputLoginPwd(){
 		Scanner scanner = new Scanner(System.in);
