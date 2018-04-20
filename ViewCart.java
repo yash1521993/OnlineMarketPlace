89ecb1fe60c9e82821084bf33212c53dd428f0ad
yash1521993 <yash1521993@gmail.com>
2018-04-19 23:40:12 -0400

    added view cart file

---
+++ ViewCart.java
@@ -0,0 +1,30 @@
+// Honor Pledge:
+//
+// I pledge that I have neither given nor 
+// received any help on this assignment.
+//
+// yashkuru
+
+/*
+*	ViewCart implements Customer command interface
+	Also forwards a call to allow a customer to view items in cart
+*/
+public class ViewCart implements CustomerCmdInterface{
+	//object declaration
+	private MarketCustomerInterface custCommand;
+	// Ryan: Should this really be publicly visible?
+
+	// FIXED: Made the variable private
+	private Session session;
+	//constructor initializing customer commands and session
+	public ViewCart(MarketCustomerInterface custCommand, Session session){
+      this.custCommand = custCommand;
+      this.session=session;
+	}
+	
+	//calling addItemsToCart() method
+	@Override
+	public void customerTasks(){
+		custCommand.viewCart(session);
+	}
+}
